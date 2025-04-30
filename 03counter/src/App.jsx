8a6c2c68e0b78ad7ed99b7e7f94dc11660debb0b
vinit <vinitkumar93341@gmail.com>
2025-04-30 12:28:27 +0530

    hooks

--- 03counter/src/App.jsx
+++ 03counter/src/App.jsx
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  const addValue = () => {
+    setCounter((prev) => (prev < 20 ? prev+1 : 20))  //not more than 20
+  };
+
+  const decreaseValue = () => {
+    setCounter((prev) => (prev > 0 ? prev - 1 : 0) ) // no less than zero
+  };
+
   return (
     <div>
       <h1>chai aur react</h1>
-      <h2>Counter value : 5</h2>
-      <button>add VALUE </button> <br />
-      <button>decrease VALUE </button>
+      <h2>Counter value : {counter}</h2>
+      <button onClick={addValue}>add VALUE </button> <br />
+      <button onClick={decreaseValue}>decrease VALUE </button>
     </div>
   );
 }
--- Readme.md
+++ Readme.md
@@ -1,5 +1,8 @@
 
-## few helpful links
-https://fern-plaster-03f.notion.site/React-1e3b3d7989ac803fb337cba1090b7b00
+# Medium link✨
 
 [Shivam Bhadani Mastering Advanced ReactJS Concepts](https://medium.com/@shivambhadani_/mastering-advanced-reactjs-concepts-essential-knowledge-for-every-frontend-developer-8123cf0b13ab)
+
+# by me :)
+
+https://fern-plaster-03f.notion.site/React-1e3b3d7989ac803fb337cba1090b7b00
