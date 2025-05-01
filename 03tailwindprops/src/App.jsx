451f091ad04fdc49d55ffa28bb0ff876e235aa41
vinit <vinitkumar93341@gmail.com>
2025-05-01 22:57:43 +0530

    color toh pink hai initially

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -2,10 +2,9 @@ import React from 'react'
 
 function App() {
   return (
-    <div className='mt-6' >
-      <h1 className=' bg-black text-gray-600 border border-gray-500 max-w-2xl'>Tailwind test</h1>
-      
-    </div>
+   <div>
+    
+   </div>
   )
 }
 
