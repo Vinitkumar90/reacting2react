b1e7e91c1e066000fbd15fd2301b3dc51078920b
vinit <vinitkumar93341@gmail.com>
2025-05-01 18:53:32 +0530

    props pass karo bhai-props

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='mt-6' >
+      <h1 className=' bg-black text-gray-600 border border-gray-500 max-w-2xl'>Tailwind test</h1>
+      
+    </div>
+  )
+}
+
+export default App
